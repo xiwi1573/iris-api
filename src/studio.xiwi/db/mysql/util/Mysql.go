3ee2494290c02abda68500ab970525baebd98c7e
xiwi <xiwi92@hotmail.com>
2017-09-04 18:05:12 +0800

    update

--- src/studio.xiwi/db/mysql/util/Mysql.go
+++ src/studio.xiwi/db/mysql/util/Mysql.go
@@ -13,7 +13,6 @@ func init() {
 
 //获取mysqldb实例
 func GetMysqlDb() (*sql.DB, error) {
-	// db, err := sql.Open("mysql", "xiwi:root123@tcp(123.207.242.90:3306)/go_api?parseTime=true")
-	db, err := sql.Open("mysql", "root:root123_@tcp(47.94.134.9:3306)/go_api?parseTime=true")
+	db, err := sql.Open("mysql", "xiwi:root123@tcp(192.168.2.90:3306)/go_api?parseTime=true")
 	return db, err
 }
